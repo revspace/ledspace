1fb096e8b46837f9f3db767e9007392ba557d2d5
f0x <f@0x52.eu>
2018-03-21 09:54:29 +0100

    semi-functional simulator

--- sim/ledbannersim.c
+++ sim/ledbannersim.c
@@ -6,8 +6,8 @@
 #include "SDL.h"
 #include "SDL_video.h"
 
-#define WIDTH 80
-#define HEIGHT 8
+#define WIDTH 36
+#define HEIGHT 36
 #define DEPTH 24
 #define PIXELS (WIDTH * HEIGHT)
 #define BYTES (PIXELS * 3)
@@ -22,22 +22,31 @@ int main(int argc, char *argv[])
     (void)argv;
 
     SDL_Init(SDL_INIT_VIDEO);
+    //SDL_Renderer *renderer;
 
     // get window surface
     SDL_Window *window = 
         SDL_CreateWindow("ledbanner", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH*BIGGER, HEIGHT*BIGGER, 0);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
     SDL_Surface *surface = SDL_GetWindowSurface(window);
 
     // create surface for led banner frame
     SDL_Surface *frame_surface = 
         SDL_CreateRGBSurfaceFrom(frame, WIDTH, HEIGHT, DEPTH, WIDTH*3, 0x0000FF, 0x00FF00, 0xFF0000, 0);
 
+    SDL_RendererFlip flip = SDL_FLIP_HORIZONTAL;
+
+
+
     // main loop
     int c;
     while ((c = read(0, frame, BYTES))) {
 
         SDL_BlitScaled(frame_surface, NULL, surface, NULL);
-        SDL_UpdateWindowSurface(window);
+        
+        SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_RenderCopyEx(renderer, texture, NULL, NULL, 90, NULL, flip);
+        SDL_RenderPresent(renderer);
         
         SDL_Event event;
         if (SDL_PollEvent(&event)) {
